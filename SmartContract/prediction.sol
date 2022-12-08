114e9404fb89ea7518db67022ebb613cff9ec25f
WEB5DEV <88079157+web5dev-official@users.noreply.github.com>
2022-12-08 10:09:38 +0530

    added contract

---
+++ SmartContract/prediction.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ShibxPredictionV1 {
+    struct Round {
+        uint256 roundid
+    }
+}
---
+++ src/blockchain/config.js
@@ -0,0 +1,8 @@
+export const Config = {
+    RpcUrl : "https://data-seed-prebsc-1-s1.binance.org:8545/",
+    PredictionSmartContarct : "",
+    ChainId : 97 ,
+    ShibxAddress : "0x3d7F1a8C3864977924529149BF50D4664FAADd35",
+    PredictionAbi : require("./prediction.json"),
+    ShibxAbi : require("./shibx.json")
+}
