09b86fb7224e580ac1164346cdc95bcf437551c0
WEB5DEV <88079157+web5dev-official@users.noreply.github.com>
2022-12-09 11:25:57 +0530

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Card() {
   return (
-    <div className='w-[40vw] h-[20vh] bg-yellow-400 rounded-[20] border-blue-800'>card</div>
+    <div className='Card-Main'>card</div>
   )
 }
