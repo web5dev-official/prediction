a6a1b35516fee696ca7a324475d8c3f7ac46def1
WEB5DEV <88079157+web5dev-official@users.noreply.github.com>
2022-12-12 05:52:47 +0530

    up

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,26 +5,37 @@ export default function Card() {
   console.log(data)
   return (
     <div className='Card-Main'>
-      
-        {
-          data.map((d)=>{
-            return(
-              <div className='Card-Heading'>
-                <div className='card-h1'>
-                  <p>
-                {d.round}
+
+      {
+        data.map((d) => {
+          return (
+            <div className='Card-Heading'>
+              <div className='card-h1'>
+                <p>
+                  {d.round}
                 </p>
+              </div>
+              <div className='ProgressBar-background'>
+                <div className='ProgressBar'>
                 </div>
-                <div className='card-h2'>
-                {d.data}
-                </div>
-                 </div>
-            )
+              </div>
+              <div className='card-h2'>
+                <div className='card-box'>
+                  <div>
+                    <p>Prize Pool :</p>
+                    <p>378 TShibx</p>
+                  </div>
+                  <button className='btn-up'>Enter Up</button>
+                  <button className='btn-down'>Enter Down</button>
+                </div> 
+              </div>
+            </div>
+          )
 
-          })
-        }
+        })
+      }
 
-     
+
     </div>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,6 @@ import "./footer.css"
 
 export default function Footer () {
   return (
-    <div className='footer'>Copyright©2022 Shibx Powered By <a href='https://web5lab.net'> WEB5LAB</a></div>
+    <div className='footer'>Copyright ©2022 Shibx Powered By <a href='https://web5lab.net'> WEB5LAB</a></div>
   )
 }
